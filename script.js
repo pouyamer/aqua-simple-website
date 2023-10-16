05d662deb68018651f8ce4d59cf7b3a6bef681cd
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-10-16 22:53:01 +0330

    added scroll percentage to nav bar

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const nav = document.querySelector(".nav")
+const hamburgerButtonEl = document.querySelector(".hamburger-button-container")
+const mobileShadowEL = document.querySelector(".mobile-shadow")
+const navInnerScrollBarEl = document.querySelector(".nav-inner-scroll-bar")
 
-document.addEventListener("keypress", e => {
-  if ((e.key = "s")) {
-    nav.classList.toggle("active")
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("hamburger-button-container")) {
+    nav.classList.add("active")
+    mobileShadowEL.classList.add("shown")
+
+    return
   }
+
+  if (nav.classList.contains("active") && !e.target.classList.contains("nav")) {
+    nav.classList.remove("active")
+    mobileShadowEL.classList.remove("shown")
+    return
+  }
+})
+
+document.addEventListener("scroll", e => {
+  let scrollPercent =
+    (window.scrollY * 100) / (document.body.offsetHeight - window.innerHeight)
+
+  if (innerWidth > 768) navInnerScrollBarEl.style.width = scrollPercent + "%"
+  else navInnerScrollBarEl.style.height = scrollPercent + "%"
 })
